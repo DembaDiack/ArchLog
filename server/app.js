0c2551363e2c2f7ede95fd265035bc4b59c8d3f8
unknown <dembadiack@outlook.com>
2021-10-06 13:28:47 +0000

    ajout connection base donne a distance, mongod db atlas

--- server/app.js
+++ server/app.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 const mainRoutes = require("./Router/Router");
-
+const Connection = require("./Database/Connection");
 app.use(mainRoutes);
 
+const connectServer = ()=>
+{
+    Connection.connect()
+    .then(()=>{
+        app.listen(PORT,()=>{
+            console.log(`app started on port ${PORT}`);
+        });
+    })
+    .catch((err)=>{
+        console.log(err);
+        connectServer();
+    })
+}
 
-app.listen(PORT,()=>{
-    console.log(`app started on port ${PORT}`);
-});
+connectServer();
