3e6273f4512d10c318259400da2f4861ee730602
unknown <dembadiack@outlook.com>
2021-10-06 13:33:09 +0000

    ajout connection mongodb atlas + kk commentaires

--- server/Database/model.js
+++ server/Database/model.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose;
 
+
+//ici on va mettre un model,des quon auras une bonne structure de base de donnee
+
 const User = new Schema({
     firstName : String,
     lastName : String,
